9a930795f214d9dad623cb3ce722ca7bb44f9f9a
Chuck Hilyard <chuck.hilyard@reachlocal.com>
2018-01-11 15:04:48 -0800

    updates

--- cmr_approval_gate/cmr_approval_gate.py
+++ cmr_approval_gate/cmr_approval_gate.py
@@ -4,32 +4,32 @@ from jira import JIRA
 
 class CmrApprovalGate():
   def __init__(self):
-    self.build_number  = os.environ['BUILD_NUMBER']
-    self.build_id      = os.environ['BUILD_ID']
-    self.build_url     = os.environ['BUILD_URL']
-    self.job_name      = os.environ['JOB_NAME']
-    self.job_base_name = os.environ['JOB_BASE_NAME']
-    self.git_commit    = os.environ['GIT_COMMIT']
-    self.jira_cmr      = os.environ['JIRA_CMR']
-    self.user          = os.environ['user']
+    self.build_display_name = os.environ['BUILD_DISPLAY_NAME']
+    self.build_number       = os.environ['BUILD_NUMBER']
+    self.run_display_url    = os.environ['RUN_DISPLAY_URL']
+    self.job_name           = os.environ['JOB_NAME']
+    self.jira_cmr           = os.environ['JIRA_CMR']
 
   def get_build_number(self):
     return self.build_number
 
   def output(self):
+    print("build display name: ", format(self.build_display_name))
     print("build number: ", format(self.build_number))
-    print("build id: ", format(self.build_id))
+    print("run display url: ", format(self.run_display_url))
     print("job name: ", format(self.job_name))
-    print("job base name: ", format(self.job_base_name))
-    print("build url: ", format(self.build_url))
-    print("git commit: ", format(self.git_commit))
     print("jira_cmr: ", format(self.jira_cmr))
-    print("user: ", format(self.user))
 
 
-class Jira():
+class Jira(cmr_number):
   def __init__(self):
     print("connecting to https://tickets.reachlocal.com")
     options = { 'server': 'https://tickets.reachlocal.com'}
     jira = JIRA(options)
-    issue = jira.issue('CMR-2924')
+
+  def find_approved_cmr(cmr_number):
+    issue = jira.search_issues('project = CMR AND status = CAB-APPROVED AND key = CMR-2924')
+    print("issue: ", issue)
+
+
+
