ce05869fd82ddb7a7f3efe6c53b02f8ff4410d94
Chuck Hilyard <chuck.hilyard@reachlocal.com>
2018-01-16 09:31:53 -0800

    updates

--- cmr_approval_gate/jira_handler.py
+++ cmr_approval_gate/jira_handler.py
@@ -9,14 +9,19 @@ class JiraHandler():
   def create_connection_handler(self):
     print("connecting to https://tickets.reachlocal.com")
     options = { 'server': 'https://tickets.reachlocal.com'}
-    return JIRA(options)
+    try:
+      jira_conn_authd = JIRA(options, basic_auth=('admin', 'youwish'))
+    return jira_conn_authd
 
   def find_approved_cmr(self, jira_conn, cmr_number):
     JQL = "project = CMR AND status = CAB-APPROVED AND key = {0} AND component = Media".format(cmr_number)
     issue = jira_conn.search_issues(JQL)
     print("issue: ", issue)
 
+  def match_build_string_from_cmr():
+    pass
+
   def add_comment_to_approved_cmr(self, jira_conn, cmr_number, build_url):
     print("adding comment to {0}".format(cmr_number))
-    jira_conn.add_comment(cmr_number, build_url)
+    jira_conn.add_comment('CMR-2926', build_url)
 
