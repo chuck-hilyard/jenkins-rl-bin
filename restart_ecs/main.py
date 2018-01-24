6ba70fa6380245dd14ef18451ed29ba9037fd198
Chuck Hilyard <chuck.hilyard@reachlocal.com>
2018-01-24 11:09:58 -0800

    fixed restart_ecs

---
+++ restart_ecs/main.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import os
+import sys
+import restart_ecs
+
+
+class Main():
+
+  def __init__(self):
+    print("Main()")
+
+  def run(self, region):
+    clustername = os.environ['JOB_NAME']
+    #ch = consul_handler.ConsulHandler()
+    #region = ch.get_key("REGION")
+    ecs = restart_ecs.RestartEcs()
+    containerids = ecs.get_ecs_containers(clustername, region)
+    ecs.restart_ecs_containers(clustername, containerids, region)
+
+
+if __name__ == '__main__':
+  main = Main()
+  main.run(sys.argv[1])
+
--- restart_ecs/restart_ecs.py
+++ restart_ecs/restart_ecs.py
@@ -1,35 +1,24 @@
-#!/usr/bin/env python3
 
 import boto3
 import json
 import re
 import os
-import consul_handler
 
-# the hammer
-def restart_ecs_containers(clustername, containerids):
-  client = boto3.client('ecs')
-  tasks = client.list_tasks(cluster=clustername)['taskArns']
-  print("TASKS: ", tasks)
-  for taskid in tasks:
-    response = client.stop_task(cluster=clustername, task=taskid, reason="deploying software")
-    print("response: ", response)
+class RestartEcs():
+  # the hammer
+  def restart_ecs_containers(self, clustername, containerids, region):
+    client = boto3.client('ecs')
+    tasks = client.list_tasks(cluster=clustername)['taskArns']
+    print("TASKS: ", tasks)
+    for taskid in tasks:
+      response = client.stop_task(cluster=clustername, task=taskid, reason="deploying software")
+      print("response: ", response)
 
-def get_ecs_containers(clustername):
-  client = boto3.client('ecs')
-  containers = client.list_container_instances(cluster=clustername)['containerInstanceArns']
-  if not containers:
-    raise Exception("no containers found in ecs cluster {0}".format(clustername))
-  else:
-    return client.list_container_instances(cluster=clustername)['containerInstanceArns']
+  def get_ecs_containers(self, clustername, region):
+    client = boto3.client('ecs')
+    containers = client.list_container_instances(cluster=clustername)['containerInstanceArns']
+    if not containers:
+      raise Exception("no containers found in ecs cluster {0}".format(clustername))
+    else:
+      return client.list_container_instances(cluster=clustername)['containerInstanceArns']
 
-
-def main(*args):
-  clustername = os.environ['JOB_NAME']
-  containerids = get_ecs_containers(clustername)
-  restart_ecs_containers(clustername, containerids)
-  #return response
-
-
-if __name__ == '__main__':
-  main()
