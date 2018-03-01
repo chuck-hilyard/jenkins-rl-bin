b783e9e926231553dcdb849a43dc6f1cfee5db57
Chuck Hilyard <chuck.hilyard@reachlocal.com>
2018-03-01 09:54:54 -0800

    updates

--- cmr_approval_gate/test/test_cmr_approval_gate.py
+++ cmr_approval_gate/test/test_cmr_approval_gate.py
@@ -9,7 +9,7 @@ class TestCmrApprovalGate(unittest.TestCase):
     self.cmr = cmr_approval_gate.CmrApprovalGate()
     os.putenv('BUILD_NUMBER', '62')
     os.putenv('BUILD_ID', '62')
-    os.putenv('BUILD_URL', 'http://10.233.72.141:8080/job/madmin-client-deploy-dev-usa/62/')
+    os.putenv('BUILD_URL', 'http://10.233.72.125:8080/job/madmin-client-deploy-dev-usa/62/')
     os.putenv('JOB_NAME', 'madmin-client-deploy-dev-usa')
     os.putenv('JOB_BASE_NAME', 'madmin-client-deploy-dev-usa')
     os.putenv('GIT_COMMIT', '7010456bb538fe4dc872126fe9e545c331fd139a')
