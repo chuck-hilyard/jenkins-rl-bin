185fb565ed62ebf060945d9e4ff7e13a4a630e79
Chuck Hilyard <chuck.hilyard@reachlocal.com>
2018-12-18 00:49:51 +0000

    localtest

--- cmr_approval_gate/vault.py
+++ cmr_approval_gate/vault.py
@@ -8,8 +8,12 @@ import os
 
 class Vault():
 
-  role_id = os.environ['role_id']
-  secret_id = os.environ['secret_id']
+  #role_id = os.environ['role_id']
+  #secret_id = os.environ['secret_id']
+  #role_id = "8b033c01-db63-5d3d-f247-29e034ecca0f"
+  #secret_id = "951d465d-3596-1286-b691-ef8ac9f0d4f4"
+  role_id = "b079f35b-4427-c676-7956-b44437d31a0d"
+  secret_id = "4d9f10bd-b62c-be6f-a55e-35b42b2f908d"
   vault_url = "http://base-camp-vault.media.dev.usa.reachlocalservices.com:8200"
 
   def __init__(self):
