56aaad4201b0463e0f0a1e125b4cfa7390528a85
Chuck Hilyard <chuck.hilyard@reachlocal.com>
2019-02-12 08:58:30 -0800

    testing

--- consul_configs/main.py
+++ consul_configs/main.py
@@ -1,8 +1,13 @@
+#!/usr/bin/env python3
+
 
 import consul_kv
 
 
-
-
 def main():
-    
+  TESTJSON = { 'TESTVALUE': 'blahblah', 'TESTVALUE2': 'blahblah2' }
+  return TESTJSON
+
+if __name__ == '__main__':
+  TEST = main()
+  print("TEST: ",  TEST)
