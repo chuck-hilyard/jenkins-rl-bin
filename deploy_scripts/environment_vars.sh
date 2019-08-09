9222a4fd06e1604f63ffb1160cd590087ad6038a
Chuck Hilyard <chuck.hilyard@reachlocal.com>
2019-08-09 14:44:47 -0700

    consul jenkins elb

--- deploy_scripts/environment_vars.sh
+++ deploy_scripts/environment_vars.sh
@@ -4,7 +4,7 @@ PROJECT="$(echo ${Job} |sed 's/\-build//g')"
 ENV_PLAT="$(echo ${JOB_NAME} |sed 's/'${PROJECT}'\-deploy\-//g')"
 ENV_DOT_PLAT="$(echo ${ENV_PLAT} |sed 's/\-/\./g')"
 
-keys=$(curl -s http://consul.${ENV_DOT_PLAT}.media.reachlocalservices.com:8500/v1/kv/${PROJECT}/config?recurse |jq '.[] | {key: .Key} | .[]'| sed 's/"//g')
+keys=$(curl -s https://consul-jenkins.${ENV_DOT_PLAT}.media.reachlocalservices.com/v1/kv/${PROJECT}/config?recurse |jq '.[] | {key: .Key} | .[]'| sed 's/"//g')
 
 VARS_FILE="${WORKSPACE}/deploy_environment_vars.txt"
 
