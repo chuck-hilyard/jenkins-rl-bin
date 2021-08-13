fdc2c38307e8636e50664579fe87284d7e3adfaf
Chuck Hilyard <chuck.hilyard@reachlocal.com>
2021-08-13 11:56:44 -0700

    update base camp vault endpoint

--- vault/vaultkey.py
+++ vault/vaultkey.py
@@ -12,7 +12,7 @@ VAULTKEY = os.environ['VAULTKEY']
 
 def create(key_type):
   headers = {'X-Vault-Token': VAULTKEY }
-  url     = "http://base-camp-vault.media.dev.usa.reachlocalservices.com:8200/v1/secret/data/{env}/{platform}/{key_type}/{unique_name}".format(env=environment, platform=platform, key_type=key_type, unique_name=unique_name)
+  url     = "https://base-camp-vault-external.media.dev.usa.reachlocalservices.com:8200/v1/secret/data/{env}/{platform}/{key_type}/{unique_name}".format(env=environment, platform=platform, key_type=key_type, unique_name=unique_name)
   file = open(thefile, 'r')
   string = file.read()
   json_formatted = json.dumps( { 'key': string } )
@@ -23,7 +23,7 @@ def get(key_type):
   # this is some wierd crap w/ py 3.6.5 on the jenkins docker build server *shrugs
   urllib3.disable_warnings()
   headers = {'X-Vault-Token': VAULTKEY }
-  url     = "http://base-camp-vault.media.dev.usa.reachlocalservices.com:8200/v1/secret/data/{env}/{platform}/{key_type}/{unique_name}".format(env=environment, platform=platform, key_type=key_type, unique_name=unique_name)
+  url     = "https://base-camp-vault-external.media.dev.usa.reachlocalservices.com:8200/v1/secret/data/{env}/{platform}/{key_type}/{unique_name}".format(env=environment, platform=platform, key_type=key_type, unique_name=unique_name)
   response = requests.get(url, headers=headers, verify=False)
   jsonstr = json.loads(response.text)
   string = jsonstr['data']['key']
