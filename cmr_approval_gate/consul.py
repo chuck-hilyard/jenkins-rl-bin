9c9d8c6c2296c64812d50bc67113ff9c4b6ff1fa
Chuck Hilyard <chuck.hilyard@reachlocal.com>
2022-04-29 15:21:45 +0000

    consul switch to v2 consul.dev.usa

--- cmr_approval_gate/consul.py
+++ cmr_approval_gate/consul.py
@@ -7,22 +7,26 @@ class Consul():
     print("Consul()")
 
   def get_role_id(self):
-    conn = consul_kv.Connection(endpoint="http://consul.media.dev.usa.reachlocalservices.com:8500/v1/")
-    target_path = "base-camp-vault/config/role_id"
+    #TODO: delete this after 06/2022
+    #conn = consul_kv.Connection(endpoint="http://consul.media.dev.usa.reachlocalservices.com:8500/v1/")
+    conn = consul_kv.Connection(endpoint="https://consul.dev.usa.media.reachlocalservices.com/v1/")
+    target_path = "jenkins-rl-bin/config/role_id"
     try:
       raw_target_path = conn.get(target_path)
-      role_id = raw_target_path['base-camp-vault/config/role_id']
+      role_id = raw_target_path['jenkins-rl-bin/config/role_id']
       return role_id
     except:
       print("problem scraping consul role_id...ignoring")
       return None
 
   def get_secret_id(self):
-    conn = consul_kv.Connection(endpoint="http://consul.media.dev.usa.reachlocalservices.com:8500/v1/")
-    target_path = "base-camp-vault/config/secret_id"
+    #TODO: delete this after 06/2022
+    #conn = consul_kv.Connection(endpoint="http://consul.media.dev.usa.reachlocalservices.com:8500/v1/")
+    conn = consul_kv.Connection(endpoint="https://consul.dev.usa.media.reachlocalservices.com/v1/")
+    target_path = "jenkins-rl-bin/config/secret_id"
     try:
       raw_target_path = conn.get(target_path)
-      secret_id = raw_target_path['base-camp-vault/config/secret_id']
+      secret_id = raw_target_path['jenkins-rl-bin/config/secret_id']
       return secret_id
     except:
       print("problem scraping consul secret_id...ignoring")
